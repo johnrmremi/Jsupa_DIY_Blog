88997d00fd8a2877e7d9b9fe8b065f89501372c8
johnrmremi <johnrmremi01@gmail.com>
2021-09-11 19:57:40 +0300

    validate and document

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
@@ -6,58 +7,58 @@ from django.contrib.auth.models import User
 
 
 class Comment(models.Model):
-    """Model representing a post comment."""
-    content = models.TextField(max_length=1000, help_text='Enter the comment of a post')
-    post = models.ForeignKey('Post', on_delete=models.RESTRICT, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    date_of_comment = models.DateTimeField(null=True, blank=True)
+    """Model representing a blog comment."""
+    description = models.TextField(max_length=1000, help_text='Enter comment about blog here.')
+    blog = models.ForeignKey('Blog', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['date_of_comment']
+        ordering = ['post_date']
 
-    def display_comment_reduced(self):
-        """Create a string for the commet. This is required to display the first 10 words of blog comment in the admin interface."""
-        if len(self.content) > 75:
-            self.content = self.content[:75]
-            return self.content + ' ...'
-        return self.content
+    def display_short_comment_description(self):
+        """Create a string for the commet desscription. This is required to display the first 75 words of blog comment description if exceeded that length."""
+        if len(self.description) > 75:
+            self.description = self.description[:75]
+            return self.description + ' ...'
+        return self.description
 
-    display_comment_reduced.short_description = 'Comment'
+    display_short_comment_description.short_description = 'Comment'
 
-    def display_post_reduced(self):
-        """Create a string for the post. This is required to display the first 10 words of blog post in the admin interface."""
-        test  =  str(self.post)
+    def display_short_blog_name(self):
+        """Create a string for the blog name. This is required to display the first 10 words of blog name in the admin interface."""
+        test  =  str(self.blog)
         if len(test) > 10:
             test = test[:10]
             return test + ' ...'
         return test
 
-    display_post_reduced.short_description = 'Post'
+    display_short_blog_name.short_description = 'Post'
 
     def __str__(self):
         """String for representing the Model object."""
-        return self.content
+        return self.description
 
-class Post(models.Model):
-    """Model representing a post."""
-    title = models.CharField(max_length=200)
+class Blog(models.Model):
+    """Model representing a blog."""
+    name = models.CharField(max_length=200)
 
-    # Foreign Key used because post can only have one (blogger)author, but bloggers can have multiple posts
+    # Foreign Key used because blog can only have one (blogger)author, but bloggers can have multiple blogs
     # Blogger as a string rather than object because it hasn't been declared yet in the file
     blogger = models.ForeignKey('Blogger', on_delete=models.SET_NULL, null=True)
 
-    content = models.TextField(max_length=1000, help_text='Enter the content of a post')
-    date_of_post = models.DateField(null=True, blank=True)
+    description = models.TextField(max_length=1000, help_text='Enter you blog text here.')
+    post_date = models.DateField(default=date.today)
 
     class Meta:
-        ordering = ['-date_of_post']
+        ordering = ['-post_date']
 
-        # Defining the permission to allow loggin in bloggers to view their blogs.
-        permissions = (("can_view_blogs", "View blogs as blogger"),)
+        # Permission for loggeed in bloggers to view, update and delete their blogs.
+        permissions = (("can_add_and_update_and_delete_blog", "View blogs as blogger"),)
 
     def __str__(self):
         """String for representing the Model object."""
-        return self.title
+        return self.name
 
     def get_absolute_url(self):
         """Returns the url to access a detail record for this post."""
@@ -65,21 +66,21 @@ class Post(models.Model):
 
 
 class Blogger(models.Model):
-    """Model representing an author(blogger)."""
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    bio = models.TextField(max_length=500, help_text='Enter the biography about the blogger')
+    """Model representing a blogger(blog author)."""
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(max_length=500, help_text='Enter your bio details here.')
 
     class Meta:
         ordering = ['author']
 
-    def display_bio_reduced(self):
+    def display_shor_bio_description(self):
         """Create a string for the bio. This is required to display the first 100 words of blogger biography in the admin interface."""
         if len(self.bio) > 100:
             self.bio = self.bio[:100]
             return self.bio + ' ...'
         return self.bio
 
-    display_bio_reduced.short_description = 'Bio'
+    display_shor_bio_description.short_description = 'Bio'
 
 
     def get_absolute_url(self):
--- blog/urls.py
+++ blog/urls.py
@@ -3,18 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('blogs/', views.PostListView.as_view(), name='blogs'),
-    path('blog/<int:pk>', views.PostDetailView.as_view(), name='blog-detail'),
+    path('blogs/', views.BlogListView.as_view(), name='blogs'),
+    path('blog/<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
     path('bloggers/', views.BloggerListView.as_view(), name='bloggers'),
     path('blogger/<int:pk>', views.BloggerDetailView.as_view(), name='blogger-detail'),
     path('comments/', views.CommentsByUserListView.as_view(), name='my-comments'),
     path('myblogs/', views.BlogsByBloggerListView.as_view(), name='my-blogs'),
-    path('blog/<int:pk>/comment/', views.CommentCreate.as_view(), name='blog_comment'),
+    path('blog/<int:pk>/comment/', views.CommentCreate.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdate.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDelete.as_view(), name='comment-delete'),
-    path('blog/create/', views.PostCreate.as_view(), name='blog-create'),
-    path('blog/<int:pk>/update', views.PostUpdate.as_view(), name='blog-update'),
-    path('blog/<int:pk>/delete', views.PostDelete.as_view(), name='blog-delete'),
+    path('blog/create/', views.BlogCreate.as_view(), name='blog-create'),
+    path('blog/<int:pk>/update', views.BlogUpdate.as_view(), name='blog-update'),
+    path('blog/<int:pk>/delete', views.BlogDelete.as_view(), name='blog-delete'),
 
 
 ]
